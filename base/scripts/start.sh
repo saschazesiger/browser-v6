c9561e7b72c526e74f5ac8d6f36ac9cf0c5c3734
saschazesiger <janis.zesiger@gmail.com>
2024-07-01 21:11:07 +0000

    fix edge

--- base/scripts/start.sh
+++ base/scripts/start.sh
@@ -34,65 +34,6 @@ screen -wipe 2&>/dev/null
 
 trap 'kill ${!}; term_handler' SIGTERM
 
-
-CUR_V="$(${DATA_DIR}/bin/microsoft-edge --version 2>/dev/null | cut -d ' ' -f3)"
-if [ "${MS_EDGE_V}" == "latest" ]; then
-  LAT_V="$(wget -qO- https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/ | grep -oP '(?<=href=").*?(?=">)' | awk -F'>' '{print $1}' | grep '^[a-zA-Z]' | sort -V | tail -1 | cut -d '_' -f2 | cut -d '-' -f1)"
-  if [ -z "${LAT_V}" ]; then
-    if [ -z "${CUR_V}" ]; then
-      echo "Something went horribly wrong with version detection!"
-	  echo "Can't get latest version and found no current installed version!"
-	  echo "Putting container into sleep mode..."
-      sleep infinity
-	else
-	  echo "Couldn't get latest version from Microsoft-Edge, falling back to installed version: ${CUR_V}"
-	  LAT_V="${CUR_V}"
-    fi
-  fi
-else
-  LAT_V="${MS_EDGE_V}"
-fi
-
-if [ -d ${DATA_DIR}/temp ]; then
-  rm -rf ${DATA_DIR}/temp
-fi
-
-if [ -z "${CUR_V}" ]; then
-  echo "---Microsoft Edge not installed, please wait installing...---"
-  mkdir -p ${DATA_DIR}/temp ${DATA_DIR}/bin
-  cd ${DATA_DIR}/temp
-  if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/temp/ms-edge-$LAT_V.deb "https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_126.0.2592.81-1_amd64.deb" ; then
-    echo "---Sucessfully downloaded Microsoft Edge---"
-  else
-    echo "---Something went wrong, can't download Microsoft Edge, putting container in sleep mode---"
-    sleep infinity
-  fi
-  ar x ${DATA_DIR}/temp/ms-edge-$LAT_V.deb
-  tar -xf ${DATA_DIR}/temp/data.tar.xz
-  mv ${DATA_DIR}/temp/opt/microsoft/msedge/* ${DATA_DIR}/bin/
-  rm -rf ${DATA_DIR}/temp
-elif [ "${CUR_V}" != "${LAT_V}" ]; then
-  echo "---Version missmatch, please wait installing latest version: ${LAT_V}...---"
-  mkdir -p ${DATA_DIR}/temp
-  cd ${DATA_DIR}/temp
-  if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/temp/ms-edge-$LAT_V.deb "https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_126.0.2592.81-1_amd64.deb" ; then
-    echo "---Sucessfully downloaded Microsoft Edge---"
-  else
-    echo "---Something went wrong, can't download Microsoft Edge, falling back to installed version: ${CUR_V}---"
-    rm -rf ${DATA_DIR}/temp
-    break
-  fi
-  rm -rf ${DATA_DIR}/bin
-  mkdir -p ${DATA_DIR}/bin
-  ar x ${DATA_DIR}/temp/ms-edge-$LAT_V.deb
-  tar -xf ${DATA_DIR}/temp/data.tar.xz
-  mv ${DATA_DIR}/temp/opt/microsoft/msedge/* ${DATA_DIR}/bin/
-  rm -rf ${DATA_DIR}/temp
-elif [ "${CUR_V}" == "${LAT_V}" ]; then
-  echo "---Microsoft Edge v${CUR_V} up-to-date---"
-fi
-
-
 su browser -c "/opt/scripts/start-server.sh" &
 killpid="$!"
 while true
